e04343b27946375ff22dd895d3fe37f6bc826b0d
sabbaman <sabbaman@gmail.com>
2023-03-14 15:29:46 +0100

    presets

--- module/settings/TraitsPresets.js
+++ module/settings/TraitsPresets.js
@@ -39,5 +39,102 @@ export const traitPresets = {
             "1": { "name": "Physical", "dice": { "value": { "0": "8" } } },
             "2": { "name": "Social", "dice": { "value": { "0": "8" } } },
         }
-    }
-  }
+    },
+    "2":{
+        "label": "Skills (Classic RPG)",
+        "hasDescription": true,
+        "description": "Skill set for a classic fantasy RPG",
+        "settings": {
+          "hasDescription": true,
+          "hasLabel": false,
+          "hasDescriptors": false,
+          "hasSfx": false,
+          "hasSubTraits": false,
+          "hasDice": true,
+          "diceConsumable": false
+        },
+        "traits": {
+          "0": {
+            "name": "Acrobatics",
+            "description": "The ability to perform feats of agility and coordination, such as tumbling, jumping, and balancing.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          },
+          "1": {
+            "name": "Athletics",
+            "description": "The ability to run, jump, swim, climb, and perform other physical feats of strength and endurance.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          },
+          "2": {
+            "name": "Combat",
+            "description": "The ability to fight with weapons or bare-handed, and to understand tactics and strategy in battle.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          },
+          "3": {
+            "name": "Crafts",
+            "description": "The ability to create and repair items using tools and raw materials, such as woodworking, metalworking, and leatherworking.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          },
+          "4": {
+            "name": "Influence",
+            "description": "The ability to persuade, deceive, and intimidate others, and to gather information through social interaction.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          },
+          "5": {
+            "name": "Knowledge",
+            "description": "The ability to recall information and make deductions based on that information, such as history, magic, and lore.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          },
+          "6": {
+            "name": "Perception",
+            "description": "The ability to notice details and perceive hidden dangers, such as traps, ambushes, and secret doors.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          },
+          "7": {
+            "name": "Stealth",
+            "description": "The ability to move silently, hide in shadows, and pick locks or pockets.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          },
+          "8": {
+            "name": "Survival",
+            "description": "The ability to live off the land, track animals and people, and navigate in wilderness environments.",
+            "dice": {
+              "value": {
+                "0": "6"
+              }
+            }
+          }
+        }
+      }
+    }      
